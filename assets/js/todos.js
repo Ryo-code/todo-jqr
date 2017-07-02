838cc4c38ebe430b4d55b6d70bcbe1412e748810
Ryo <kanadajinlee@yahoo.ca>
2017-07-02 16:53:39 -0500

    You can delete (with remove()) to-dos now

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,4 +3,10 @@ $('li').click(function(){
   $(this).toggleClass("completed");
 });
 
-
+//click on X to delete to-do
+$("span").click(function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  }); //the parent of the span you clicked on is the li
+  event.stopPropagation();; //jQuery method that stops event bubbling
+});
